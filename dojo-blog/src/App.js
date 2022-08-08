e1b06a09c942a96f1d8268f4df0524921637ade1
efosblack <eriamiatoeefe11@gmail.com>
2022-08-08 08:46:56 -0400

    initial commit

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  const title = 'welcome to the new blog';
+  const likes = 50;
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked { likes } times</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
