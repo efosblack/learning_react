b5fd183c16f2d0f6215df1eb4fa49d5595580163
efosblack <eriamiatoeefe11@gmail.com>
2022-08-08 08:57:04 -0400

    dynamic values in Templates

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 function App() {
   const title = 'welcome to the new blog';
   const likes = 50;
+  const link = "http://www.google.com";
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
         <p>Liked { likes } times</p>
+        <p>{ 10 }</p>
+        <p>{ "hello guys" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
