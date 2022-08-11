05440fe41fe848557235becfcc575dd5c3904a26
efosblack <eriamiatoeefe11@gmail.com>
2022-08-11 02:43:57 -0400

    click events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('hello, blaq');
+    }
+
+    const handleClickagain = (name) => {
+        console.log('hello ' + name );
+    }
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickagain('luigi')}>Click me again</button>
         </div>
     );
 }
