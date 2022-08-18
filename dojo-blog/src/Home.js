6d44ad5e2a8ef4730c8a15af7f4712ac7f34a062
efosblack <eriamiatoeefe11@gmail.com>
2022-08-18 17:11:08 -0400

    conditional loading message

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,9 +3,10 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
  
 
-      const [name, setName] = useState('mario');
+      
 
 
 
@@ -17,11 +18,13 @@ const Home = () => {
           .then(data => {
             console.log(data)
             setBlogs(data);
+            setIsPending(false);
         });
       }, []);
 
     return (
         <div className="home">
+          {isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
  
            
