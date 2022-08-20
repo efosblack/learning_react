cc6359fcebaea0b31716478de252ea0bac5e592d
efosblack <eriamiatoeefe11@gmail.com>
2022-08-20 11:32:10 -0400

    sending post requests

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -4,12 +4,23 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
 
-        console.log(blog);
+        setIsPending(true);
+
+        fetch('http://localhost:7800/blogs', {
+            method: 'POST',
+            headers: { "content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })
+
     }
 
     return ( 
@@ -38,7 +49,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button>}
+                {  isPending && <button disabled>Adding Blog</button>}
                 <p>{ title }</p>
                 <p>{ body }</p>
                 <p>{ author }</p>
